a4e15041edaa386a4dbe49877b1e7c827fd4a5f8
Vikash Kumar <mailkumarvikash@gmail.com>
2017-11-15 22:54:47 +0530

    update setup script to compile YouCompleteMe and node

--- setup.sh
+++ setup.sh
@@ -13,7 +13,47 @@ cat vim_config.vim > ~/.vim_runtime/my_configs.vim
 echo "Installing all plugins using Vundle"
 vim +PluginInstall +qall
 
+echo "checking if homebrew present"
+if ! hash brew 2>/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "checking if wget present, installing if not"
+if ! hash wget 2>/dev/null; then
+    brew install wget --with-libressl
+fi
+
+echo "installing cmake, required for YouCompleteMe"
+wget -qO- https://cmake.org/files/v3.10/cmake-3.10.0-rc5.tar.gz | tar xvz -C ~/Downloads
+cd ~/Downloads/cmake-3.10.0-rc5
+./bootstrap
+make
+make install
+cd ..
+rm -rf cmake-3.10.0-rc5
+
+echo "switching to YouCompleteMe"
+cd ~/.vim_runtime/my_plugins/YouCompleteMe
+./install.py --clang-completer
+
+echo "source vimrc file"
+vim vim -c ~/.vimrc
+
 echo "putting tmux config file to tmux_config.conf"
 cat tmux_config.conf > ~/.tmux.conf
+
+echo "source tmux file"
+tmux source ~/.tmux.conf
+
+echo "installing node js"
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+mkdir ~/local
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=~/local
+make install # ok, fine, this step probably takes more than 30 seconds...
+curl https://www.npmjs.org/install.sh | sh
 
 echo "Everything done!! Enjoy."
